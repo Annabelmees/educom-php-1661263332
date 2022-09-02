3fd2d23b58ac41ed369043b19c6083ee61680540
Annabelmees <annabel01.meester@gmail.com>
2022-09-02 10:43:20 +0200

    PHP Switch (#1)

---
+++ php-1-switch.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    switch ($color) {
+        case "red":
+            echo "Hello";
+            break;
+        case "green":
+            echo "Welcome";
+            break;
+    }
+    ?>
+</body>
+</html>
